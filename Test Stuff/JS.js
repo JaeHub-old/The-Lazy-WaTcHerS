9c5009827dd6efbbed46297ef5ed77362592f04d
Jae the Supreme <Jaekim06789@gmail.com>
2018-07-18 23:18:34 -0700

    Added a New Sprite

--- Test Stuff/JS.js
+++ Test Stuff/JS.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(1000, 550);
 var mainState = {
     preload: function() {
-        game.load.image('player','assets/NewPiskel.png', 32, 32);
+        game.load.spritesheet('player','assets/characters_7.png', 32, 32);
         game.load.image('mountains-back','assets/mountains-back.png');
         game.load.image('mountains-mid1','assets/mountains-mid1.png');
         game.load.image('mountains-mid2','assets/mountains-mid2.png');
@@ -43,6 +43,10 @@ var mainState = {
         this.cursors = game.input.keyboard.createCursorKeys();
         
         this.player = game.add.sprite(0, 400,'player');
+        //sets the player's anchor to the center
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.scale.setTo(2);
+        this.player.frame = 0;
 
         this.playerOldPos = 0
 
@@ -57,19 +61,35 @@ var mainState = {
             this.mountainsMid2.tilePosition.x += 1.2;
         }
         this.player.body.velocity.x = -150;
-    }
-    
-    else if (this.cursors.right.isDown) {
+    }else if (this.cursors.right.isDown) {
         if(this.playerOldPos != this.player.body.x) {
             this.mountainsBack.tilePosition.x -= 0.3;
             this.mountainsMid1.tilePosition.x -= 0.7;
             this.mountainsMid2.tilePosition.x -= 1.2;
         }
         this.player.body.velocity.x = +150;
+    }else{
+        this.player.body.velocity.x = 0;
     }
-//       this.playerOldPos = this.player.body.x 
-       
     
+    if(this.cursors.up.isDown){
+        if(this.playerOldPos != this.player.body.y){
+            this.mountainsBack.tilePosition.y -= 0.3;
+            this.mountainsMid1.tilePosition.y -= 0.7;
+            this.mountainsMid2.tilePosition.y -= 1.2;
+        }
+        this.player.body.velocity.y = -200;
+    }else if(this.cursors.down.isDown){
+        if(this.playerOldPos != this.player.body.y) {
+            this.mountainsBack.tilePosition.y += 0.3;
+            this.mountainsMid1.tilePosition.y += 0.7;
+            this.mountainsMid2.tilePosition.y += 1.2;
+        }
+        this.player.body.velocity.y = +200;
+    }else{
+        this.player.body.velocity.y = 0;
+    }
+       
     }
 }
     
