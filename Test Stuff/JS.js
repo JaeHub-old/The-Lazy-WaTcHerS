c4c5c9b5c48f92466e28f5b9accdec0baafc8d58
Jae the Supreme <Jaekim06789@gmail.com>
2018-07-23 09:06:18 -0700

    Made Tilemap Test :D

--- Test Stuff/JS.js
+++ Test Stuff/JS.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(1000, 540);
 var mainState = {
     preload: function() {
-        game.load.spritesheet('player','assets/characters_7.png', 32, 35);
+        game.load.spritesheet('player','assets/characters_7.png', 32, 32.5);
         game.load.image('mountains-back','assets/mountains-back.png');
         game.load.image('mountains-mid1','assets/mountains-mid1.png');
         game.load.image('mountains-mid2','assets/mountains-mid2.png');
@@ -10,22 +10,6 @@ var mainState = {
     },
     
     create: function() {
-        
-        //Effective Map Object
-        this.map_01 = this.game.add.tilemap('map_01');
-        this.map_01.addTilesetImage('tiles', 'mapTiles');
-        
-        //Background and ForeGround
-        this.bgLayer = this.map_01.createLayer('Background');
-        this.wallsLayer = this.map_01.createLayer('Walls');
-        
-        //Collision Tiles
-        this.map_01.setCollision([5, 6, 7, 8, 90, 91, 92], true, this.wallsLayer);
-        
-        //Arcade Physics
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        
-        game.world.enableBody = true;
         
         //The Test's Background Color
         game.stage.backgroundColor = '#697e96';
@@ -36,7 +20,7 @@ var mainState = {
             game.cache.getImage('mountains-back').height,
             'mountains-back'
         );
-//        this.mountainsBack.fixedToCamera = true;
+       this.mountainsBack.fixedToCamera = true;
         
         this.mountainsMid1 = game.add.tileSprite(0,
             game.height - game.cache.getImage('mountains-mid1').height,
@@ -44,7 +28,7 @@ var mainState = {
             game.cache.getImage('mountains-mid1').height,
             'mountains-mid1'
         );
-//        this.mountainsMid1.fixedToCamera = true; 
+       this.mountainsMid1.fixedToCamera = true; 
             
         this.mountainsMid2 = game.add.tileSprite(0,
             game.height - game.cache.getImage('mountains-mid2').height,
@@ -52,18 +36,35 @@ var mainState = {
             game.cache.getImage('mountains-mid2').height,
             'mountains-mid2'
         );
-//        this.mountainsMid2.fixedToCamera = true;
+       this.mountainsMid2.fixedToCamera = true;
+        
+        //Arcade Physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        game.world.enableBody = true;
+        
+        //Effective Map Object
+        this.map_01 = this.game.add.tilemap('map_01');
+        this.map_01.addTilesetImage('Platform_tilesets', 'mapTiles');
+        
+        //Background and ForeGround
+        this.bgLayer = this.map_01.createLayer('Background');
+       this.wallsLayer = this.map_01.createLayer('Walls');
+        
+        //Collision Tiles
+        this.map_01.setCollision([21, 22, 23, 24, 38, 39, 40, 41, 55, 56, 57, 58, 72, 73, 74, 75, 89, 90, 91], true, this.wallsLayer);
+        
             
         this.cursors = game.input.keyboard.createCursorKeys();
         
         this.player = game.add.sprite(0, 400,'player');
         //sets the player's anchor to the center
-        this.player.anchor.setTo(0.5, 0.5);
+//        this.player.anchor.setTo(0.5, 0.5);
         this.player.scale.setTo(2);
         this.player.frame = 24;
         this.playerOldPos = 0;
         
-        game.world.setBounds(0, 0, 1024, 576);
+        game.world.setBounds(0, 0, 4000, 800);
         game.camera.follow(this.player); 
         
         this.player.animations.add('right', [23, 24, 25, 26], 10, true);
@@ -89,17 +90,17 @@ var mainState = {
     }else{
         if(this.cursors.left.isDown){
         if(this.playerOldPos != this.player.body.x){
-            this.mountainsBack.tilePosition.x += 0.3;
-            this.mountainsMid1.tilePosition.x += 0.7;
-            this.mountainsMid2.tilePosition.x += 1.2;
+            this.mountainsBack.tilePosition.x += 0.5;
+            this.mountainsMid1.tilePosition.x += 0.9;
+            this.mountainsMid2.tilePosition.x += 1.4;
         }
         this.player.body.velocity.x = -150;
         this.player.animations.play('right');
     }else if (this.cursors.right.isDown) {
         if(this.playerOldPos != this.player.body.x) {
-            this.mountainsBack.tilePosition.x -= 0.3;
-            this.mountainsMid1.tilePosition.x -= 0.7;
-            this.mountainsMid2.tilePosition.x -= 1.2;
+            this.mountainsBack.tilePosition.x -= 0.5;
+            this.mountainsMid1.tilePosition.x -= 0.9;
+            this.mountainsMid2.tilePosition.x -= 1.4;
         }
         this.player.body.velocity.x = +150;
         this.player.animations.play('right');
@@ -108,10 +109,8 @@ var mainState = {
     }
     
     if(this.cursors.up.isDown){
-        this.player.body.velocity.y = -200;
+        this.player.body.velocity.y = -400;
         this.player.animations.play('jump');
-    }else if(this.cursors.down.isDown){
-        this.player.body.velocity.y = +200;
     }else{
             this.player.body.velocity.y = 0;
          }
--- Test Stuff/assets/Platform_tilesets.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Platform_tilesets" tilewidth="32" tileheight="32" tilecount="442" columns="17">
- <image source="../Platform_tilesets.png" width="544" height="832"/>
-</tileset>
